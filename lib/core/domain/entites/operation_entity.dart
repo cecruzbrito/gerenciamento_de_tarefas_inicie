86f8f78421db99c9452c1a34953ab340cb7bb03b
cecruzbrito <broududs@gmail.com>
2025-09-26 11:05:21 -0300

    fix(core): removendo a classe não mais utilizada 'operationentity'

--- lib/core/domain/entites/operation_entity.dart
+++
@@ -1,7 +0,0 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-class OperationEntity {
-  final bool hasSuccess;
-  final String msg;
-  final Function()? onTap;
-  OperationEntity({required this.hasSuccess, required this.msg, this.onTap});
-}
